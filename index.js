bb37dcd16c18c803fe53e74319b3b0d4b5b9d4d0
Philip Tronser <tronser@web.de>
2022-01-19 13:36:55 +0100

    variables

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const rows = "20";
+const cols = "30";
+const cellWidth = canvas.width / cols;
+const cellHeight = canvas.height / rows;
+let snake = [
+  { x: 2, y: 3 },
+  { x: 3, y: 3 },
+];
+let food = { x: 5, y: 5 };
+let direction = "LEFT";
 
 draw();
 
 function draw() {
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'white';
-    add(100, 150);
-    add(130, 150);
-    add(160, 150);
-    add(190, 150);
-   
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
 
-    ctx.fillStyle = 'yellow';
-    add(100, 100);
+  snake.forEach(part => add(part.x, part.y))
+  
+  ctx.fillStyle = "yellow";
+  add(food.x, food.y);
 }
 
 function add(x, y) {
-    ctx.fillRect(x, y, 30 -1, 30 -1);
+  ctx.fillRect(x * cellWidth, y * cellHeight, cellWidth -1, cellHeight - 1);
 }
 
-function gameLoop() {
-
-}
-
+function gameLoop() {}
