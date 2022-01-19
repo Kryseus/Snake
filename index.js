e0a4c606bcfabfbc71af6268f4d3b6de28051373
Philip Tronser <tronser@web.de>
2022-01-19 13:08:05 +0100

    functions

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ draw();
 
 function draw() {
     ctx.fillStyle = 'black';
-    ctx.fillRect(10, 20, 20, 20);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'white';
+    add(100, 150);
+    add(130, 150);
+    add(160, 150);
+    add(190, 150);
+   
+
+    ctx.fillStyle = 'yellow';
+    add(100, 100);
+}
+
+function add(x, y) {
+    ctx.fillRect(x, y, 30 -1, 30 -1);
 }
 
 function gameLoop() {
 
 }
+
